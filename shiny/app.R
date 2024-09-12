a850039cd8daded858afa3743030e25c5707f8c5
Randy Johnson <Randall.Johnson@nih.gov>
2024-09-12 16:10:13 -0400

    Add hello world shiny app

---
+++ shiny/app.R
@@ -0,0 +1,21 @@
+# app.R
+# Main JoesFlowHPC app
+
+# library(JoesFlowHPC)
+#
+# load_JoesFlow_config('Skyline_dev', 'inst/config.yml')
+
+library(shiny)
+
+ui <- fluidPage(
+  textOutput("user_name")
+)
+
+server <- function(input, output, session) {
+  output$user_name <- renderText({
+    paste0("Hello ", Sys.getenv("USER"), "!")
+    #paste("Logged in as:", user_info$username)
+  })
+}
+
+shinyApp(ui = ui, server = server)
